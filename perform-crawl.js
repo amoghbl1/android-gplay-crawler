3130f2eea9c3dfcbd38b60f2afc11279787ee1a2
Amogh Pradeep <amoghbl1@gmail.com>
2023-03-28 16:00:30 -0400

    Update crawler

--- perform-crawl.js
+++ perform-crawl.js
@@ -19,14 +19,18 @@ const readline = require('readline');
 const exec     = require('shelljs').exec;
 const util     = require('util');
 const pexec    = util.promisify( exec );
-const adb      = require('adbkit');
+const adb      = require('@u4/adbkit');
 
 ( async() => {
   // Interfaces needed for Android
   const client = adb.createClient();
   const devices = await client.listDevices();
+  if (!devices.length) {
+    console.error('Need at least one connected android device');
+    return;
+  }
   // Moving to working with just one device
-  const device = devices[0];
+  const deviceClient = devices[0].getClient();
 
   // Setup all required folders
   await createDirectories([APKS_BASE_DIR, APKS_DIR])
@@ -36,31 +40,64 @@ const adb      = require('adbkit');
 
   console.log(`Downloading ${packagesToDownload.length} apks`);
 
+  // Drive phone to play and get it to download the apps in sequence
   for (let package_name of packagesToDownload) {
-    console.log(`Trying to download package: ${package_name}`);
+    consoleDebug(`Trying to download package: ${package_name}`);
+    // Hit download for all APKs, with a minimal sleep, tinker as required
+    await maybeDownloadFromPlay(deviceClient, package_name);
+  }
 
-    await downloadFromPlay(client, device.id, package_name);
-
+  // Extract each package in sequence
+  for (let package_name of packagesToDownload) {
+    consoleDebug(`Pulling ${package_name}`);
+    await pullAPKFromPhone(deviceClient, package_name);
   }
 
 })();
 
-async function downloadFromPlay(client, id, package_name) {
+async function pullAPKFromPhone(deviceClient, package_name) {
+  const apkPieces = (await deviceClient.execOut(`pm path ${package_name}`, 'utf8'))
+                  .split("\n").filter(e => typeof e === 'string' && e !== '')
+  let splitNumber = 0;
+  for (let apkPiece of apkPieces) {
+    let pieceDestination = `${APKS_DIR}/${package_name}-${splitNumber}.apk`;
+    apkPiece = apkPiece.substring(8) // drop 'package:'
+    consoleDebug(`Pulling ${apkPiece} to ${pieceDestination}`);
+    // TODO: Pull file and dump it to the disk
+    splitNumber += 1;
+  }
+  consoleDebug(`Done pulling ${package_name}, total ${splitNumber} pieces`);
+}
 
-  await client.shell(id, `am start -a android.intent.action.VIEW -d market://details?id=${package_name}`)
-    .catch(async () => {
-      console.log(`Failed to launch play store for package: ${package_name}`);
+async function maybeDownloadFromPlay(deviceClient, package_name) {
+
+  // Check if package is installed already
+  let installed = await deviceClient.isInstalled(package_name);
+  if (installed) {
+    consoleDebug(`Package ${package_name} already installed, skipping...`);
+    return;
+  }
+
+  await deviceClient.startActivity({
+      'action': 'android.intent.action.VIEW',
+      'data': `market://details?id=${package_name}`,
+      'wait': true
     });
   await sleep(2000);
 
-  // X = 600, Y = 780
-  await client.shell(id, `input tap 600 780`);
-  await sleep(5000);
+  // X = 600, Y = 780 for our device
+  await deviceClient.extra.tap('600', '780');
+  await sleep(2000);
 }
 
 function readPackagesFile() {
   return fs.readFileSync(PACKAGE_NAMES_FILE, 'utf8').split("\n")
-            .filter(e => typeof e === 'string' && e !== '' && !e.startsWith("#"))
+            .filter(e =>
+                typeof e === 'string' &&
+                e !== '' &&
+                !e.startsWith("#") &&
+                e !== 'X'
+              )
 }
 
 async function createDirectories(directories) {

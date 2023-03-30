b91d64762c8694ac297eab1837a766258468a7d7
Amogh Pradeep <amoghbl1@gmail.com>
2023-03-30 18:16:43 -0400

    Use shell adb pull to copy over the .apks

--- perform-crawl.js
+++ perform-crawl.js
@@ -57,13 +57,19 @@ const adb      = require('@u4/adbkit');
 
 async function pullAPKFromPhone(deviceClient, package_name) {
   const apkPieces = (await deviceClient.execOut(`pm path ${package_name}`, 'utf8'))
-                  .split("\n").filter(e => typeof e === 'string' && e !== '')
+                    .split("\n").filter(e => typeof e === 'string' && e !== '')
   let splitNumber = 0;
+  let apkSHA = '';
   for (let apkPiece of apkPieces) {
-    let pieceDestination = `${APKS_DIR}/${package_name}-${splitNumber}.apk`;
     apkPiece = apkPiece.substring(8) // drop 'package:'
+    if (apkSHA === '') {
+      apkSHA = (await deviceClient.execOut(`sha256sum ${apkPiece}`, 'utf8'))
+                .split(" ").filter(e => typeof e === 'string' && e!== '')[0]
+    }
+    let pieceDestination = `${APKS_DIR}/${package_name}-${apkSHA}-${splitNumber}.apk`;
     consoleDebug(`Pulling ${apkPiece} to ${pieceDestination}`);
-    // TODO: Pull file and dump it to the disk
+    // Too lazy to write the adbkit version of this, just going with shell
+    await pexec(`adb pull ${apkPiece} ${pieceDestination}`);
     splitNumber += 1;
   }
   consoleDebug(`Done pulling ${package_name}, total ${splitNumber} pieces`);
